aec50f81a3587721d063ee04f1a3e94db1648f5a
Paloma Almonte <palomaalmontez@gmail.com>
2023-09-04 20:02:30 -0400

    Initial commit

---
+++ M9-Challenge-Almonte-Paloma/authorization-service/src/main/java/com/company/authorizationservice/controller/UserInfoController.java
@@ -0,0 +1,16 @@
+package com.company.authorizationservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserInfoController {
+    @RequestMapping(value = "/userinfo", method = RequestMethod.GET)
+    public Principal getUserInfo(Principal principal){
+        System.out.println("Called userinfo");
+        return principal;
+    }
+}
